920084bc4762223977774972251202ba8766274c
IsmailKemmoune <i.kemmoune@gmail.com>
2022-06-24 11:40:01 +0100

    created api controllers for the manager post

--- api/routes/api-routes.js
+++ api/routes/api-routes.js
@@ -1,39 +1,25 @@
-const ManagerPost = require("../model/managerPost")
 const router = require('express').Router()
+const {
+	getAllManagerPosts,
+	getSingleManagerPost,
+	createManagerPost,
+	editManagerPost,
+	deleteManagerPost
+} = require('../controllers/managerPostController')
 
 //GET all managers post
-router.get("/manager-post", async(req, res) => {
-	res.send("get all posts");
-})
+router.get("/manager-post", getAllManagerPosts)
 
-//GET 1 manager post
-router.get("/manager-post/:id", async(req, res) => {
-	const id = req.params.id;
-	console.log(id);
-	res.send(`get post : ${id}`);
-})
+//GET a single manager post
+router.get("/manager-post/:id", getSingleManagerPost)
 
 //POST a manager post
-router.post("/manager-post", async (req, res) => {
- const {description, payment, energy, split: slpSplit, slp: slpQuota, title} = req.body;
-try {
-	const managerPost = await ManagerPost.create({description, payment, energy, slpSplit, slpQuota, title});
-	res.status(200).json(managerPost)
-} catch (error) {
-	res.status(400).json({error: error.message});
-}
-})
+router.post("/manager-post", createManagerPost)
 
 //EDIT a manager post
-router.put("/manager-post/:id", async(req, res) => {
-	const id = req.params.id;
-	res.send(`update post : ${id} `);
-})
+router.patch("/manager-post/:id", editManagerPost)
 
 //DELETE a manager post
-router.delete("/manager-post/:id", async(req, res) => {
-	const id = req.params.id;
-	res.send(`delete post : ${id}`);
-})
+router.delete("/manager-post/:id", deleteManagerPost)
 
 module.exports = router
